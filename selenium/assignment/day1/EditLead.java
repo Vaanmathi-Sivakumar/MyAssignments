0b0c012b13515a171edda78ff9bcd471cd81d6dd
Vaanmathi-Sivakumar <91522031+Vaanmathi-Sivakumar@users.noreply.github.com>
2023-03-14 22:53:19 +0530

    Add files via upload

---
+++ selenium/assignment/day1/EditLead.java
@@ -0,0 +1,57 @@
+package selenium.assignment.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EditLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EdgeDriver driver =new EdgeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize(); //To maximize the window/browser size
+		
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		
+		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Vaanmathi");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Sivakumar");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Vaan");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Tester");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("Vaan@gmail.com");
+		WebElement soureDropDown=driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select dropDown =new Select(soureDropDown);
+		dropDown.selectByVisibleText("New York");
+		driver.findElement(By.className("smallSubmit")).click();
+		
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_description")).clear();
+		driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Updated lead");
+		driver.findElement(By.name("submitButton")).click();
+		
+		String actualResult="View Lead";
+		
+		String expectedResult=driver.getTitle();
+		
+		if(actualResult.equals(expectedResult))
+		{
+			System.out.println("create Lead created successfully");
+		}
+		else
+		{
+			System.out.println("create Lead not created successfully");
+		}
+	
+
+
+	}
+
+}
